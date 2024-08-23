424ae9d54cdb42544bd6bd99afd283c855c703ba
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-08-23 13:46:02 +0200

    delete and update setup

--- backend/src/controllers/taskroutes.ts
+++ backend/src/controllers/taskroutes.ts
@@ -22,3 +22,32 @@ export const get_task = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const update_task = async (req: Request, res: Response) => {
+  try {
+    const { name, description, status, id } = req.body;
+    const updatetodo = await Todo.findByIdAndUpdate(
+      { _id: id },
+      { name, description, status }
+    );
+    const allTodos = await Todo.find();
+    res
+      .status(201)
+      .json({
+        message: "updated a todo",
+        todo: updatetodo,
+        allTodos: allTodos,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const delete_task = async (req: Request, res: Response) => {
+  const { id } = req.body;
+  const deletedTask = await Todo.findByIdAndDelete({ _id: id });
+  const allTodos = await Todo.find();
+  res
+    .status(201)
+    .json({ message: "updated a todo", todo: deletedTask, allTodos: allTodos });
+};
