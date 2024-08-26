b6ebc4eda9d7c7caf884ef7f0997ed53d6bfb6a0
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-08-26 14:35:11 +0200

    model setup

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,7 @@
 import { Response, Request } from "express";
-export const login_get = (req: Request, res: Response) => {};
+export const login_get = (req: Request, res: Response) => {
+  res.redirect("/login");
+};
 
 export const login_post = (req: Request, res: Response) => {};
 
---
+++ backend/src/models/user.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+import isEmail from "validator/lib/isEmail";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "please enter the username"],
+  },
+  password: {
+    type: String,
+    required: [true, "please enter the password"],
+
+    minLength: [6, "the password is not six characters"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: [isEmail, "your email is not valid"],
+  },
+});
+
+const User = model("User", userSchema);
