d1fed2d96a19a68327545d067379c69bcbe666b8
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-09-30 10:10:17 +0200

    helod

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,19 @@ import keys from "./keys";
 import taskRoutes from "./routes/taskRoutes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
 
 const app: Express = express();
-mongoose.connect(
-  "mongodb+srv://pascal:tasktrack@tasktrack.9wydl.mongodb.net/?retryWrites=true&w=majority&appName=tasktrack"
-);
+dotenv.config();
+try {
+  mongoose.connect(
+    "mongodb+srv://pascal:tasktrack@tasktrack.9wydl.mongodb.net/?retryWrites=true&w=majority&appName=tasktrack"
+  );
+  console.log("Connected to the database");
+} catch (error) {
+  console.log("an error occured while connecting");
+}
 const corsOptions = {
   origin: "https://task-track-peach.vercel.app/",
   credentials: true,
@@ -19,6 +26,6 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("app is running on");
 });
