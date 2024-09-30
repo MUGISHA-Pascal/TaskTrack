3bd2b0ba362b2a01a8929d79d6cf850c39f7807a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-09-30 08:56:17 +0200

    uploading

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 import keys from "./keys";
 import taskRoutes from "./routes/taskRoutes";
-// import cors from "cors";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes";
 
@@ -10,13 +10,13 @@ const app: Express = express();
 mongoose.connect(
   "mongodb+srv://pascal:tasktrack@tasktrack.9wydl.mongodb.net/?retryWrites=true&w=majority&appName=tasktrack"
 );
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: "https://task-track-peach.vercel.app/",
+  credentials: true,
+};
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors());
+app.use(cors(corsOptions));
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 app.listen(4000, () => {
