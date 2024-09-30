7e896ba6e5fea805493469a0e2f8cfced0dae95a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-09-30 08:19:09 +0200

    uploading

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,16 +7,18 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes";
 
 const app: Express = express();
-mongoose.connect(keys.mongoURL);
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-};
+mongoose.connect(
+  "mongodb+srv://pascal:tasktrack@tasktrack.9wydl.mongodb.net/?retryWrites=true&w=majority&appName=tasktrack"
+);
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true,
+// };
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 app.listen(4000, () => {
-  console.log("app is running on http://localhost:4000");
+  console.log("app is running on");
 });
